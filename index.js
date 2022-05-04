9eb66eb7c92872e63d0ebb5bd1baad16ba66d270
River <+>
2022-05-03 23:33:12 -0400

    commit tree 6227825cc8d64cc4927d97a74dfdbc75189c7610

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const submitData= function(userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify({
+            "name":userName,
+            "email":userEmail
+        })
+    })
+    .then(response=> response.json())
+    .then(function(object) {
+        const newDiv = document.createElement("div");
+        newDiv.innerHTML = object.id;
+        document.body.appendChild(newDiv);
+    })
+    .catch((error)=> {
+        const errorDiv=document.createElement("div")
+        errorDiv.innerHTML=error.message;
+        document.body.append(errorDiv);
+})
+}
